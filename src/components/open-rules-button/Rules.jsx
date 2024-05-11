07b38a43e45a913ee78c7c8c1286422b2394cd8e
Ivan <my_lord@macbook.local>
2024-05-10 23:04:18 -0700

    Responsive

--- src/components/open-rules-button/Rules.jsx
+++ src/components/open-rules-button/Rules.jsx
@@ -1,6 +1,7 @@
 import './Rules.css';
 
 import { useState } from 'react';
+import ExamplePicture from '../../ExampleForRules.png';
 
 import { FaQuestion, FaArrowRight } from 'react-icons/fa';
 
@@ -16,11 +17,12 @@ const Rules = () => {
         <h2>Rules</h2>
         <p>
           This is a game like Tic Tac Toe but there are nine grids instead of
-          just one. Winning a grid earns a point and transforms the entire grid
-          into a big sign representing the winning player. The objective is to
-          accumulate the most points or achieve three big signs in a row to win
-          the game.
+          just one. Winning a small grid earns a point and transforms the small
+          grid into a big sign representing the winning player. If you win the
+          big grid you earn three points and game finishes. The objective is to
+          get the most points.
         </p>
+        <img src={ExamplePicture} alt='' />
         <div className='close-rules-btn' onClick={() => setIsOpen(false)}>
           <FaArrowRight />
         </div>
